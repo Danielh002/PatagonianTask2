1c9654b2188865607d3bfaeea80ee78cf349c2e8
Daniel <danielhernandez002@hotmail.com>
2021-04-18 23:36:57 -0500

    Removing unnecesary route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,5 @@ const controller = require('./controller');
 
 
 module.exports = function (app, upload) {
-    app.route('/').get((req, res) => res.send("Hola adios"));
     app.post('/api/uploadfile', upload, controller.upload)
 }
